c68de134825dd42c876dea8115c9bf6cdbaefa06
Nonamerz <a.g.kolesnik@yandex.ru>
2021-11-16 00:17:08 +0300

    Initial Commit

---
+++ NewsAppUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsAppUIKit
+//
+//  Created by MacBook Pro on 16/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
